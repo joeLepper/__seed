5ecbf3ce2d52b2b392f6d81d5949701041472686
Joe Lepper <jlepper@newrelic.com>
2013-11-06 10:15:35 -0800

    creates hellow world directive

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,20 @@
 
 var App = angular.module('App', []);
 
-App.controller('HelloCtrl', function HelloCtrl($scope) {
-  $scope.yourName = "your name";
-  $scope.alertName = function(){
-    alert($scope.yourName);
-  };
+App.directive('hello', function HelloDir() {
+  return{
+    controller: function($scope){
+        $scope.yourName = "your name";
+        $scope.alertName = function(){
+          alert($scope.yourName);
+        };
+      },
+    template:
+      '<form id="hello_form" ng-submit="alertName()">' +
+        '<input type="text" ng-model="yourName" placeholder="Enter your name here">' +
+        '<h1>Hello {{yourName}}! It is nice to see you.</h1>' +
+        '<button type="submit" form="hello_form">Submit</button>' +
+      '</form>',
+    restrict: 'E'
+  }
 });
