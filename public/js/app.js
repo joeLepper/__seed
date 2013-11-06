be52afe995e5251635a943347e698c930313fbe1
Joe Lepper <jlepper@newrelic.com>
2013-11-06 12:49:19 -0800

    puts it all together

--- public/js/app.js
+++ public/js/app.js
@@ -4,18 +4,23 @@ var App = angular.module('App', []);
 
 App.directive('hello', function HelloDir() {
   return{
-    controller: function($scope){
+    controller: function($scope, $rootScope){
         $scope.yourName = "your name";
-        $scope.alertName = function(){
-          alert($scope.yourName);
+        $scope.clicked = function(){
+          $rootScope.$broadcast('alertAll');
         };
+        $scope.alertName = function(){
+          alert('hello ' + $scope.yourName + ' from element ' + $scope.number);
+        };
+        $scope.$on('alertAll', function(){
+          $scope.alertName();
+        });
       },
     template:
-      '<form id="hello_form" ng-submit="alertName()">' +
         '<input type="text" ng-model="yourName" placeholder="Enter your name here">' +
         '<h1>Hello {{yourName}}! It is nice to see you.</h1>' +
-        '<button type="submit" form="hello_form">Submit</button>' +
-      '</form>',
-    restrict: 'E'
+        '<button ng-click="clicked()">Submit</button><br/>',
+    restrict: 'E',
+    scope: { number: '=' }
   }
 });
